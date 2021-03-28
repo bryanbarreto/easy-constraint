5514a0e79ba36639a32c8a79f54667901b335482
Bryan Barreto <bryan.barreto@icloud.com>
2021-03-27 22:55:12 -0300

    creating anchor function

--- Sources/EasyConstraint/EasyConstraint.swift
+++ Sources/EasyConstraint/EasyConstraint.swift
@@ -1,2 +1,53 @@
 private struct EasyConstraint {
 }
+
+
+
+import UIKit
+
+// MARK: - UIView Extension
+public extension UIView {
+    
+    func anchor(
+        top: NSLayoutYAxisAnchor? = nil,
+        left: NSLayoutXAxisAnchor? = nil,
+        right: NSLayoutXAxisAnchor? = nil,
+        bottom: NSLayoutYAxisAnchor? = nil,
+        
+        paddingTop: CGFloat = 0,
+        paddingLeft: CGFloat = 0,
+        paddingRight: CGFloat = 0,
+        paddingBottom: CGFloat = 0,
+        
+        width: CGFloat? = nil,
+        height: CGFloat? = nil
+    ){
+        
+        self.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let left = left {
+            self.leftAnchor.constraint(equalTo: left, constant: paddingLeft).isActive = true
+        }
+        
+        if let right = right {
+            self.rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let width = width {
+            self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+}
